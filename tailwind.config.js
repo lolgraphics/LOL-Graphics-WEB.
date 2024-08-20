a3a0b4c1cf64aca581cbae4ec7f46b78d36572bd
Paulo Guilherme <paulinhu611@gmail.com>
2024-08-20 20:40:02 -0300

    fix tailwind css config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./{src,public}/**/*.{html,js,ts,jsx,tsx}", "./src/pages/*.tsx", "./src/components/*.tsx", {raw: '<div class=min-h-screen', extension: 'html' }],
   theme: {
     extend: {},
   },
