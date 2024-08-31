15f6d331161b03efe4080b8477e68dfa8129ebac
Paulo Guilherme <paulinhu611@gmail.com>
2024-08-31 18:28:18 -0300

    teste commit

--- webpack.dev.cjs
+++ webpack.dev.cjs
@@ -8,7 +8,7 @@ module.exports = (env) =>
     mode: 'development',
     entry: './src/main.tsx',
     output: {
-      filename: 'bundle.[contenthash].js',
+      filename: 'bundle.[contenthash].js', 
       path: path.resolve(__dirname, 'dist'),
     },
     module: {
