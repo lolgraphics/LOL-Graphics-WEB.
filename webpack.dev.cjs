cb47a6db1147caa846ded21b0ed2ecbcb366acfe
Paulo Guilherme <paulinhu611@gmail.com>
2024-08-31 22:37:06 -0300

    FEA-42 commit

--- webpack.dev.cjs
+++ webpack.dev.cjs
@@ -10,7 +10,7 @@ module.exports = (env) =>
     output: {
       filename: 'bundle.[contenthash].js', 
       path: path.resolve(__dirname, 'dist'),
-    },
+    }, 
     module: {
       rules: [
         {
