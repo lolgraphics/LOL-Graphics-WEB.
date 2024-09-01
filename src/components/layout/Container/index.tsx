3d7f2e9010e94f90360a75fd326cb9b30153a285
Paulo Guilherme <paulinhu611@gmail.com>
2024-08-31 21:37:17 -0300

    commit

--- src/components/layout/Container/index.tsx
+++ src/components/layout/Container/index.tsx
@@ -2,11 +2,11 @@ import { ReactNode } from 'react';
 
 import classnames from 'classnames';
 
-export interface ContainerProps {
+export type ContainerProps = {
   children: ReactNode;
   className: string;
 }
-export default function Container({ children, className = ""}: ContainerProps) {
+export default function Container({ children, className}: Readonly<ContainerProps>) {
   const classNames = classnames('h-screen', className);
   return <div data-testid="layout-container" className={classNames}>{children}</div>;
 }
